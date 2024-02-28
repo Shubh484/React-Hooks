932937ed406fe548ed4653f932a35660a26ebc08
Shubh <shubhsingh8767@gmail.com>
2024-02-29 00:02:35 +0530

    done with ( useEffect )

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { Hello } from "./Hello";
 import { useForm } from "./useForm";
+import { useFetch } from "./useFetch";
 
 function App() {
   const [values, handleChange] = useForm({ email: "", password: "" });
 
-  useEffect(() => console.log("render"), [values.password]);
+  // http://numberapi.com/44/trivia
+
+  useFetch("http://numberapi.com/44/trivia");
+
+  // const [showHello, setShowHello] = useState(true);
+
+  // useEffect(() => console.log("render"), [values.password]);
+
+  // useEffect(() => {
+  //   const onMouseMove = (e) => {
+  //     console.log(e);
+  //   };
+  //   window.addEventListener("mousemove", onMouseMove);
+
+  //   return () => {
+  //     window.removeEventListener("mousemove", onMouseMove);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    console.log("mount1");
+  }, []);
+
+  useEffect(() => {
+    console.log("mount2");
+  }, []);
 
   return (
     <>
+      {/* <button onClick={() => setShowHello(!showHello)}>toggle</button>
+      {showHello && <Hello />} */}
       <input name="email" value={values.email} onChange={handleChange}></input>
       <input
         type="password"
